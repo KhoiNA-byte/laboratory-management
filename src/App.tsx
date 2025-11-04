0da7ea22462d324b0308f69a491ba4c5b211d9c6
shadowpro50 <khoinguyenanh205@gmail.com>
2025-11-04 08:55:43 +0700

    fix authorization and user management

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,62 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AppLayout } from './layout/AppLayout'
-import { ProtectedRoute } from './auth/ProtectedRoute'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppLayout } from "./layout/AppLayout";
+import { ProtectedRoute } from "./auth/ProtectedRoute";
 
 // Dashboard Module
-import { DashboardPage } from './modules/dashboard/DashboardPage'
+import { DashboardPage } from "./modules/dashboard/DashboardPage";
 
 // Home Module
-import { HomePageWrapper } from './modules/home/HomePageWrapper'
-import { HomePageLoggedIn } from './modules/home/HomePageLoggedIn'
+import { HomePageWrapper } from "./modules/home/HomePageWrapper";
+import { HomePageLoggedIn } from "./modules/home/HomePageLoggedIn";
 
 // IAM Module
-import { LoginPage } from './modules/iam/LoginPage'
-import { VerifyOTPPage } from './modules/iam/VerifyOTPPage'
-import { ResetPasswordPage } from './modules/iam/ResetPasswordPage'
-import { UsersPage } from './modules/iam/UsersPage'
-import { RolesPage } from './modules/iam/RolesPage'
-import UserInfoPage from './modules/iam/UserInfoPage'
+import { LoginPage } from "./modules/iam/LoginPage";
+import { VerifyOTPPage } from "./modules/iam/VerifyOTPPage";
+import { ResetPasswordPage } from "./modules/iam/ResetPasswordPage";
+import { UsersPage } from "./modules/iam/UsersPage";
+import { RolesPage } from "./modules/iam/RolesPage";
+import UserInfoPage from "./modules/iam/UserInfoPage";
 
 // Warehouse Module
-import InstrumentsPage from './modules/warehouse/InstrumentsPage'
-import { ReagentsPage } from './modules/warehouse/ReagentsPage'
-import { WarehousePage } from './modules/warehouse/WarehousePage'
-import { FlaggingRulesPage } from './modules/warehouse/FlaggingRulesPage'
-import InstrumentDetailsPage from './modules/warehouse/InstrumentDetailsPage'
-import EditInstrumentPage from './modules/warehouse/EditInstrumentPage'
-import AddInstrumentPage from './modules/warehouse/AddInstrumentPage'
+import InstrumentsPage from "./modules/warehouse/InstrumentsPage";
+import { ReagentsPage } from "./modules/warehouse/ReagentsPage";
+import { WarehousePage } from "./modules/warehouse/WarehousePage";
+import { FlaggingRulesPage } from "./modules/warehouse/FlaggingRulesPage";
+import InstrumentDetailsPage from "./modules/warehouse/InstrumentDetailsPage";
+import EditInstrumentPage from "./modules/warehouse/EditInstrumentPage";
+import AddInstrumentPage from "./modules/warehouse/AddInstrumentPage";
 
 // Test Order Module
-import { TestOrdersPage } from './modules/testorder/TestOrdersPage'
-import { TestResultPage } from './modules/testorder/TestResultPage'
-import MyTestResultsPage from './modules/testorder/MyTestResultsPage'
-import TestOrderDetailsPage from './modules/testorder/TestOrderDetailsPage'
-import UpdateTestOrderPage from './modules/testorder/UpdateTestOrderPage'
-import NewTestOrderPage from './modules/testorder/NewTestOrderPage'
+import { TestOrdersPage } from "./modules/testorder/TestOrdersPage";
+import { TestResultPage } from "./modules/testorder/TestResultPage";
+import MyTestResultsPage from "./modules/testorder/MyTestResultsPage";
+import TestOrderDetailsPage from "./modules/testorder/TestOrderDetailsPage";
+import UpdateTestOrderPage from "./modules/testorder/UpdateTestOrderPage";
+import NewTestOrderPage from "./modules/testorder/NewTestOrderPage";
 
 // Monitoring Module
-import { MonitoringPage } from './modules/monitoring/MonitoringPage'
-import { HL7MessagesPage } from './modules/monitoring/HL7MessagesPage'
-import { QuarantinePage } from './modules/monitoring/QuarantinePage'
-import { InstrumentLogsPage } from './modules/monitoring/InstrumentLogsPage'
+import { MonitoringPage } from "./modules/monitoring/MonitoringPage";
+import { HL7MessagesPage } from "./modules/monitoring/HL7MessagesPage";
+import { QuarantinePage } from "./modules/monitoring/QuarantinePage";
+import { InstrumentLogsPage } from "./modules/monitoring/InstrumentLogsPage";
 
 // Patient Module
-import { PatientsPage } from './modules/patient/PatientsPage'
-import { PatientDetailsPage } from './modules/patient/PatientDetailsPage'
-import { EditPatientPage } from './modules/patient/EditPatientPage'
+import { PatientsPage } from "./modules/patient/PatientsPage";
+import { PatientDetailsPage } from "./modules/patient/PatientDetailsPage";
+import { EditPatientPage } from "./modules/patient/EditPatientPage";
 
 // Audit Module
-import { AuditLogsPage } from './modules/audit/AuditLogsPage'
-import { ReportsPage } from './modules/audit/ReportsPage'
+import { AuditLogsPage } from "./modules/audit/AuditLogsPage";
+import { ReportsPage } from "./modules/audit/ReportsPage";
 
 // Community Module
-import { CommunityPage } from './modules/community/CommunityPage'
+import { CommunityPage } from "./modules/community/CommunityPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/verify-otp" element={<VerifyOTPPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
@@ -63,70 +64,304 @@ function App() {
         <Route path="/landing" element={<HomePageWrapper />} />
         <Route path="/community" element={<CommunityPage />} />
 
-        {/* User Home Route - Standalone page without AppLayout */}
-        <Route path="/home" element={
-          <ProtectedRoute allowedRoles={['user']}>
-            <HomePageLoggedIn />
-          </ProtectedRoute>
-        } />
+        {/* User Home Route */}
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute allowedRoles={["user"]}>
+              <HomePageLoggedIn />
+            </ProtectedRoute>
+          }
+        />
 
-        {/* Admin Dashboard Routes - With AppLayout */}
-        <Route path="/admin" element={
-          <ProtectedRoute allowedRoles={['admin']}>
-            <AppLayout />
-          </ProtectedRoute>
-        }>
-          {/* Dashboard Route - For admins */}
-          <Route index element={<DashboardPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
+        {/* Shared Admin Dashboard Layout (all roles can enter) */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute
+              allowedRoles={["admin", "manager", "lab_user", "service", "user"]}
+            >
+              <AppLayout />
+            </ProtectedRoute>
+          }
+        >
+          {/* Dashboard Routes (restricted to higher roles) */}
+          <Route
+            index
+            element={
+              <ProtectedRoute
+                allowedRoles={["admin", "manager", "lab_user", "service"]}
+              >
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute
+                allowedRoles={["admin", "manager", "lab_user", "service"]}
+              >
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* IAM Routes */}
-          <Route path="users" element={<UsersPage />} />
-          <Route path="roles" element={<RolesPage />} />
-          <Route path="user-info" element={<UserInfoPage />} />
+          <Route
+            path="users"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "manager"]}>
+                <UsersPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="roles"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <RolesPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="user-info"
+            element={
+              <ProtectedRoute
+                allowedRoles={["admin", "manager", "lab_user", "service"]}
+              >
+                <UserInfoPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Warehouse Routes */}
-          <Route path="instruments" element={<InstrumentsPage />} />
-          <Route path="instruments/new" element={<AddInstrumentPage />} />
-          <Route path="instruments/:instrumentId/edit" element={<EditInstrumentPage />} />
-          <Route path="instruments/:instrumentId" element={<InstrumentDetailsPage />} />
-          <Route path="warehouse" element={<WarehousePage />} />
-          <Route path="reagents" element={<ReagentsPage />} />
-          <Route path="flagging-rules" element={<FlaggingRulesPage />} />
+          <Route
+            path="instruments"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user", "service"]}>
+                <InstrumentsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="instruments/new"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <AddInstrumentPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="instruments/:instrumentId/edit"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <EditInstrumentPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="instruments/:instrumentId"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user", "service"]}>
+                <InstrumentDetailsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="warehouse"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <WarehousePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="reagents"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <ReagentsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="flagging-rules"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <FlaggingRulesPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Test Order Routes */}
-          <Route path="test-orders" element={<TestOrdersPage />} />
-          <Route path="test-orders/new" element={<NewTestOrderPage />} />
-          <Route path="test-orders/:orderId/edit" element={<UpdateTestOrderPage />} />
-          <Route path="test-orders/:orderId" element={<TestOrderDetailsPage />} />
-          <Route path="test-results" element={<TestResultPage />} />
-          <Route path="test-results/:orderNumber" element={<TestResultPage />} />
-          <Route path="my-test-results" element={<MyTestResultsPage />} />
+          <Route
+            path="test-orders"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "manager", "lab_user"]}>
+                <TestOrdersPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="test-orders/new"
+            element={
+              <ProtectedRoute allowedRoles={["lab_user"]}>
+                <NewTestOrderPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="test-orders/:orderId/edit"
+            element={
+              <ProtectedRoute allowedRoles={["lab_user"]}>
+                <UpdateTestOrderPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="test-orders/:orderId"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "manager", "lab_user"]}>
+                <TestOrderDetailsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="test-results"
+            element={
+              <ProtectedRoute
+                allowedRoles={["admin", "manager", "lab_user", "service"]}
+              >
+                <TestResultPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="my-test-results"
+            element={
+              <ProtectedRoute allowedRoles={["user"]}>
+                <MyTestResultsPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Monitoring Routes */}
-          <Route path="monitoring" element={<MonitoringPage />} />
-          <Route path="hl7-messages" element={<HL7MessagesPage />} />
-          <Route path="quarantine" element={<QuarantinePage />} />
-          <Route path="instrument-logs" element={<InstrumentLogsPage />} />
+          <Route
+            path="monitoring"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "service"]}>
+                <MonitoringPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="hl7-messages"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <HL7MessagesPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="quarantine"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <QuarantinePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="instrument-logs"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "service"]}>
+                <InstrumentLogsPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Patient Routes */}
-          <Route path="patients/:id/edit" element={<EditPatientPage />} />
-          <Route path="patients/:id" element={<PatientDetailsPage />} />
-          <Route path="patients" element={<PatientsPage />} />
+          <Route
+            path="patients"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "manager", "lab_user"]}>
+                <PatientsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="patients/:id"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "manager", "lab_user"]}>
+                <PatientDetailsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="patients/:id/edit"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "lab_user"]}>
+                <EditPatientPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Audit Routes */}
-          <Route path="audit-logs" element={<AuditLogsPage />} />
-          <Route path="reports" element={<ReportsPage />} />
+          <Route
+            path="audit-logs"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "manager"]}>
+                <AuditLogsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="reports"
+            element={
+              <ProtectedRoute allowedRoles={["admin", "manager"]}>
+                <ReportsPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Settings Route */}
-          <Route path="settings" element={<div className="p-6"><h1 className="text-2xl font-bold">Settings</h1><p>Settings page coming soon...</p></div>} />
+          <Route
+            path="settings"
+            element={
+              <ProtectedRoute
+                allowedRoles={["admin", "manager", "lab_user", "service"]}
+              >
+                <div className="p-6">
+                  <h1 className="text-2xl font-bold">Settings</h1>
+                  <p>Settings page coming soon...</p>
+                </div>
+              </ProtectedRoute>
+            }
+          />
 
-          {/* My Profile Route */}
-          <Route path="profile" element={<div className="p-6"><h1 className="text-2xl font-bold">My Profile</h1><p>Profile page coming soon...</p></div>} />
+          {/* My Profile Route — all roles can access */}
+          <Route
+            path="profile"
+            element={
+              <ProtectedRoute
+                allowedRoles={[
+                  "admin",
+                  "manager",
+                  "lab_user",
+                  "service",
+                  "user",
+                ]}
+              >
+                <div className="p-6">
+                  <h1 className="text-2xl font-bold">My Profile</h1>
+                  <p>Profile page coming soon...</p>
+                </div>
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,56 +1,117 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { User } from '../types'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../types";
 
 interface UserState {
-    users: User[]
-    loading: boolean
-    error: string | null
+  users: User[];
+  loading: boolean;
+  error: string | null;
+  createSuccess: boolean;
+  updateSuccess: boolean;
+  successMessage: string | null;
 }
 
 const initialState: UserState = {
-    users: [],
-    loading: false,
-    error: null,
-}
+  users: [],
+  loading: false,
+  error: null,
+  createSuccess: false,
+  updateSuccess: false,
+  successMessage: null,
+};
 
 const userSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-        fetchUsersStart: (state) => {
-            state.loading = true
-            state.error = null
-        },
-        fetchUsersSuccess: (state, action: PayloadAction<User[]>) => {
-            state.loading = false
-            state.users = action.payload
-            state.error = null
-        },
-        fetchUsersFailure: (state, action: PayloadAction<string>) => {
-            state.loading = false
-            state.error = action.payload
-        },
-        addUser: (state, action: PayloadAction<User>) => {
-            state.users.push(action.payload)
-        },
-        updateUser: (state, action: PayloadAction<User>) => {
-            const index = state.users.findIndex(user => user.id === action.payload.id)
-            if (index !== -1) {
-                state.users[index] = action.payload
-            }
-        },
-        deleteUser: (state, action: PayloadAction<string>) => {
-            state.users = state.users.filter(user => user.id !== action.payload)
-        },
+  name: "users",
+  initialState,
+  reducers: {
+    // Clear messages
+    clearMessages: (state) => {
+      state.error = null;
+      state.successMessage = null;
+      state.createSuccess = false;
+      state.updateSuccess = false;
     },
-})
+
+    // Clear specific success
+    clearCreateSuccess: (state) => {
+      state.createSuccess = false;
+      state.successMessage = null;
+    },
+    clearUpdateSuccess: (state) => {
+      state.updateSuccess = false;
+      state.successMessage = null;
+    },
+
+    // Fetch
+    getUsersRequest: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    getUsersSuccess: (state, action: PayloadAction<User[]>) => {
+      state.loading = false;
+      state.users = action.payload;
+    },
+    getUsersFailure: (state, action: PayloadAction<string>) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    // Create
+    createUserRequest: (state, _action: PayloadAction<User>) => {
+      state.loading = true;
+      state.error = null;
+      state.createSuccess = false;
+    },
+    createUserSuccess: (state, action: PayloadAction<User>) => {
+      state.loading = false;
+      state.users.push(action.payload);
+      state.createSuccess = true;
+      state.successMessage = "User created successfully!";
+    },
+    createUserFailure: (state, action: PayloadAction<string>) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.createSuccess = false;
+    },
+
+    // Update
+    updateUserRequest: (state, action: PayloadAction<User>) => {
+      state.loading = true;
+      state.error = null;
+      state.updateSuccess = false;
+    },
+    updateUserSuccess: (state, action: PayloadAction<User>) => {
+      state.loading = false;
+      const idx = state.users.findIndex((u) => u.id === action.payload.id);
+      if (idx !== -1) {
+        // Properly update the user with all fields including status
+        state.users[idx] = { ...state.users[idx], ...action.payload };
+      }
+      state.updateSuccess = true;
+      state.successMessage = `User ${
+        action.payload.status === "active" ? "activated" : "deactivated"
+      } successfully!`;
+    },
+    updateUserFailure: (state, action: PayloadAction<string>) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.updateSuccess = false;
+    },
+  },
+});
 
 export const {
-    fetchUsersStart,
-    fetchUsersSuccess,
-    fetchUsersFailure,
-    addUser,
-    updateUser,
-    deleteUser,
-} = userSlice.actions
-export default userSlice.reducer
+  clearMessages,
+  clearCreateSuccess,
+  clearUpdateSuccess,
+  getUsersRequest,
+  getUsersSuccess,
+  getUsersFailure,
+  createUserRequest,
+  createUserSuccess,
+  createUserFailure,
+  updateUserRequest,
+  updateUserSuccess,
+  updateUserFailure,
+} = userSlice.actions;
+
+export default userSlice.reducer;
